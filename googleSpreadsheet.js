364b87fe4657e44c073be7983c604337869ec5c0
Dane Stuckel <dane.stuckel@gmail.com>
2015-01-18 20:09:41 -0500

    can now pull data

---
+++ googleSpreadsheet.js
@@ -0,0 +1,105 @@
+var Promise = require('bluebird'),
+  _ = require('lodash'),
+  rest = require('restler');
+
+_.mixin(require("lodash-deep"));
+
+var googleDocApi = 'https://spreadsheets.google.com/feeds/cells/[key]/od6/public/values?alt=json';
+var cellProp = 'gs$cell';
+
+/**
+ * @param key
+ * @returns {Promise}
+ */
+function getSpreadsheet(key) {
+  var deferred = Promise.defer();
+  rest.get(googleDocApi.replace('[key]', key)).on('complete', function (result) {
+    if (result instanceof Error) {
+      deferred.reject(result);
+    } else {
+      var entries = _.deepGet(result, 'feed.entry');
+      entries = _.groupBy(_.pluck(entries, cellProp), 'row');
+      deferred.resolve(mapColumnsToHeader(groupByRow(entries)));
+    }
+  });
+  return deferred.promise;
+}
+
+function groupByRow(entries){
+  return _.reduce(entries, function (list, value, rowIndex) {
+    //value is [{}]
+    var row = _.reduce(value, function (row, value) {
+      //value is {col: string, row: string, $t: string}
+      var colIndex = parseInt(value.col, 10);
+      if (colIndex != undefined && !isNaN(colIndex)) {
+        row[colIndex - 1] = value['$t'];
+      }
+      return row;
+    }, []);
+    rowIndex = parseInt(rowIndex, 10);
+    if (rowIndex != undefined && !isNaN(rowIndex)) {
+      list[rowIndex - 1] = row;
+    }
+    return list;
+  }, []);
+}
+
+function mapColumnsToHeader(rows) {
+  var headers = rows.shift();
+
+  rows = _.map(rows, function (row) {
+    return _.zipObject(headers, row);
+  });
+
+  rows = _.map(rows, function (row) {
+    return _.pick(row, function (value) {
+      return !!value;
+    })
+  });
+  return rows;
+}
+
+function categorize(list, categories) {
+  if (categories.length) {
+    categories = _.clone(categories);
+    var category = categories.shift();
+    var grouping =_.groupBy(list, category);
+    list = _.mapValues(grouping, function (group) {
+      return categorize(group, categories);
+    });
+  }
+  return list;
+}
+
+var GoogleSpreadsheet = function () {};
+GoogleSpreadsheet.prototype = {
+  getList: function (options) {
+    return getSpreadsheet(options.key)
+      .then(function (list) {
+        return categorize(list, options.categories || []);
+      })
+  },
+  getListByProperty: function (options, property) {
+    return this.getList(options).then(function (list) {
+      return _.indexBy(list, property);
+    })
+  },
+  removeSuffix: function (obj, suffix) {
+    var newObj = _.isArray(obj) ? [] : {};
+
+    _.each(obj, function (value, key) {
+      var index = key.indexOf(suffix);
+      if (index > -1) {
+        var newKey = key.substring(0, index);
+        if (newKey) {
+          newObj[newKey] = value;
+        }
+      } else {
+        newObj[key] = value;
+      }
+    });
+
+    return newObj;
+  }
+};
+module.exports = new GoogleSpreadsheet();
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,54 @@
 var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  _ = require('lodash'),
+  Promise = require('bluebird'),
   hbs = require('gulp-static-handlebars'),
   aside = require('gulp-aside'),
   cssmin = require('gulp-cssmin'),
   rename = require('gulp-rename'),
-  data = require('./data.json');
+  data = require('./data.json'),
+  pkg = require('./package.json'),
+  googleSpreadsheet = require('./googleSpreadsheet'),
+  fs = Promise.promisifyAll(require('fs')),
+  isNewData = false;
 
 /**
  * Pull data from source and save it to new-data.json
  */
-gulp.task('pull', function () {
+gulp.task('pull', function (done) {
+  return Promise.join(
+    googleSpreadsheet.getListByProperty(pkg.config.detail, 'name'),
+    googleSpreadsheet.getList(pkg.config.singleMenu),
+    googleSpreadsheet.getList(pkg.config.groupMenu)
+  ).spread(function (details, singleMenu, groupMenu) {
+      details = _.mapValues(details, 'value');
 
+      details.singleMenu = singleMenu;
+      details.groupMenu = groupMenu;
+      return fs.writeFileAsync('new-data.json', JSON.stringify(details));
+    });
 });
 
 /**
  * Successful if new-data.json is different than data.json
  */
 gulp.task('compare', function() {
-
+  return Promise.join(
+    fs.readFileAsync('new-data.json'),
+    fs.readFileAsync('data.json')
+  ).spread(function (newData, data) {
+      isNewData = !_.isEqual(newData, data);
+      gutil.log('isNewData:', isNewData ? gutil.colors.green(isNewData): gutil.colors.red(isNewData));
+    })
 });
 
 /**
  * Copy new-data.json to data.json
  */
 gulp.task('accept', function () {
-
+  return fs.readFileAsync('new-data.json').then(function (data) {
+    return fs.writeFileAsync('data.json', data);
+  });
 });
 
 /**
